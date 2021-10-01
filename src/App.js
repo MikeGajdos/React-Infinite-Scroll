7819356a991df6d357742db6d1422f2f1f3e2006
Mike Gajdos <mikegajdos81@gmail.com>
2021-10-01 12:51:25 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useRef, useCallback } from "react";
+import useSearch from "./useBookSearch";
 function App() {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNUmber] = useState(1);
+  const { characters, hasMore, loading, error } = useSearch(query, pageNumber);
+
+  const observer = useRef();
+  const lastCharacterElement = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNUmber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
+
+  function handleChange(e) {
+    setQuery(e.target.value);
+    setPageNUmber(1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" value={query} onChange={handleChange}></input>
+      {characters.map((character, index) => {
+        if (characters.length === index + 1) {
+          return (
+            <div ref={lastCharacterElement} className="avatar" key={character}>
+              {character}
+            </div>
+          );
+        } else {
+          return (
+            <div className="avatar" key={character}>
+              {character}
+            </div>
+          );
+        }
+      })}
+      <div>{loading && "loading ..."}</div>
+      <div>{error && "Error"}</div>
+    </>
   );
 }
 
---
+++ src/useBookSearch.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function useSearch(query, pageNumber) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [characters, setCharacters] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
+  useEffect(() => {
+    setCharacters([]);
+  }, [query]);
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    const CancelToken = axios.CancelToken;
+    let cancel;
+    axios({
+      method: "GET",
+      url: `https://rickandmortyapi.com/api/character/`,
+      params: {
+        name: query,
+        page: pageNumber,
+      },
+      cancelToken: new CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => {
+        console.log(res);
+        setCharacters((prevCharacters) => {
+          return [
+            ...new Set([
+              ...prevCharacters,
+              ...res.data.results.map((item) => item.name),
+            ]),
+          ];
+        });
+        setHasMore(pageNumber < res.data.info.pages);
+        setLoading(false);
+      })
+      .catch((e) => {
+        console.log(e);
+        if (axios.isCancel(e)) return;
+        setError(true);
+      });
+    return () => cancel();
+  }, [query, pageNumber]);
+  return { loading, error, characters, hasMore };
+}
